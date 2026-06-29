28f90f9370a50da8e4818daf0d6762b66aa142aa
jignesh54 <96325918+jignesh54@users.noreply.github.com>
2026-06-29 09:15:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,121 @@
-index.html:
-script.js:
-const observer = new IntersectionObserver(entries=>{
- entries.forEach(entry=>{
-   if(entry.isIntersecting){
-      entry.target.classList.add("show");
-   }
- });
+
+const observer = new IntersectionObserver(
+(entries)=>{
+
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+
 });
-document.querySelectorAll('.fade').forEach(el=>{
- observer.observe(el);
+
+
 });
+
+
+
+document
+.querySelectorAll(".fade")
+.forEach(
+(el)=>observer.observe(el)
+);
+
+
+
+
+
+
 const roles=[
- "Flutter Developer",
- "Android Developer",
- "AI Driven Mobile Engineer"
+
+"Flutter Developer",
+
+"Android Kotlin Developer",
+
+"AI Powered Mobile Engineer"
+
 ];
-let roleIndex=0;
-let charIndex=0;
+
+
+
+let role=0;
+
+let char=0;
+
+
 const typing=document.getElementById("typing");
+
+
+
 function type(){
- if(!typing) return;
- if(charIndex<roles[roleIndex].length){
-   typing.innerHTML+=roles[roleIndex].charAt(charIndex);
-   charIndex++;
-   setTimeout(type,100);
- }else{
-   setTimeout(erase,1500);
- }
+
+
+if(char < roles[role].length){
+
+
+typing.innerHTML += roles[role][char];
+
+char++;
+
+
+setTimeout(type,100);
+
+
 }
+
+else{
+
+
+setTimeout(erase,1500);
+
+
+}
+
+
+}
+
+
+
+
 function erase(){
- if(charIndex>0){
-   typing.innerHTML=roles[roleIndex].substring(0,charIndex-1);
-   charIndex--;
-   setTimeout(erase,50);
- }else{
-   roleIndex=(roleIndex+1)%roles.length;
-   setTimeout(type,500);
- }
+
+
+if(char>0){
+
+
+typing.innerHTML =
+roles[role].substring(0,char-1);
+
+
+char--;
+
+
+setTimeout(erase,50);
+
+
 }
+
+else{
+
+
+role=(role+1)%roles.length;
+
+
+setTimeout(type,500);
+
+
+}
+
+
+
+}
+
+
+
+
 window.onload=type;
